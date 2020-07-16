47061737819e8c99fd53783cb5a109390697bc2e
Fibonacci <dave.green1664@yahoo.co.uk>
2020-07-16 16:56:36 +0100

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+	std::vector<int> nums(10, 5);
+
+	for (auto iter = nums.begin(); iter != nums.end(); ++iter)
+	{
+		*iter *= 2;
+
+		std::cout << *iter << ", ";
+	}
+
+	return 0;
+}
